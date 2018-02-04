71c04f303bc078cb898abf325316cfd01a34a3ac
Todor Angelov <tondy@tondy.com>
2018-02-04 18:30:11 +0100

    v0.0.2

--- test/server.js
+++ test/server.js
@@ -8,40 +8,68 @@
 const ts = require('abv-ts')('abv:spa.test');
 
 const Aspa = require('../index.js');
-const Response = require('../lib/Response.js');
-const aspa = new Aspa();
 
 const $port = 8080;
 const $host = '0.0.0.0';
 const $root = __dirname + '/public';
 const $start = Date.now();
+const $logs = [];
 
-aspa.set('root',$root);
-aspa.set('cache',3);
+const aspa = new Aspa({root: $root, cache: 3});
 
 // fallback if no index.html
 aspa.on('/',(req, res) => {
 		res.end('Aspa server: ' + req.url);
 	});
-	
+
 aspa.on('/time/',(req, res) => {
-		const r = aspa.res(200, req.url, Date.now() + ': ' + req.url);
+		const r = aspa.res(200, req.url, Date.now() + ': ' + req.url);ts.debug(r.size);
 		r.send(res);
 	});
 
 aspa.on('/stat/',(req, res) => {
-		let body = '<h3>Uptime: ' + ts.time(Date.now() - $start) + '</h3>';
-		const f = aspa.stat(aspa.name);
-		body += '<p><b>Cache</b><br />Timeout: ' + (aspa.get('cache')/1000);
-		body += '<br />Files: ' + f.size + '</p>';
+		aspa.auth('aspa','pass',req,res, () => {
+		let body = '<h3>' + aspa.link('/','Uptime') + ': ';
+		body += ts.time(Date.now() - $start) + '</h3>';
+		const f = aspa.stat(aspa.sign);
+		body += '<p><b>Cache</b><br />Timeout: ' + (aspa.opt.cache/1000);
+		body += ' sec<br />Files: ' + f.size + '</p>';
 		for(let [k,v] of f.entries()) body += aspa.link(k) + '<br />'; 
-		const r = aspa.res(200, req.url, aspa.page('Stats',body));
+// logs		
+		body += '<br><h3>Logs: ' + $logs.length + ' lines</h3>';
+		const clr = ['','','gray','green','red','blue'];
+		let c, s;
+		for (let l of $logs){
+			c = Math.round(l.code/100);
+			s = ' ' + l.host  + ' [' + (new Date(l.time).toUTCString()) + 
+				'] "GET ' + l.url + '" '+ l.code +' ' + l.size;
+			body += '<span style="color:'+clr[c]+'">' + s + '</span><br>';
+		}
+		const meta = '<meta http-equiv="refresh" content="30">';
+		const r = aspa.res(200, req.url, aspa.page('Stats',body,meta));
+		r.send(res);
+		});
+	});
+
+aspa.on('/logs/',(req, res) => {
+		let body = aspa.link('/','Home') + '<br><br>';
+		body += '<h3>Logs: ' + $logs.length + ' lines</h3>';
+		const clr = ['','','gray','green','red','blue'];
+		let c, s;
+		for (let l of $logs){
+			c = Math.round(l.code/100);
+			s = ' ' + l.host  + ' [' + (new Date(l.time).toUTCString()) + 
+				'] "GET ' + l.url + '" '+ l.code +' ' + l.size;
+			body += '<span style="color:'+clr[c]+'">' + s + '</span><br>';
+		}
+		const r = aspa.res(200, req.url, aspa.page('Logs',body));
 		r.send(res);
 	});
 
 aspa.tpl('/index.html',{ttl: () => {return Date.now();}});
 
 aspa.log = (l) => {
+		$logs.push(l);
 		const s = ' ' + l.host  + ' [' + (new Date(l.time).toUTCString()) + 
 			'] "GET ' + l.url + '" '+ l.code +' ' + l.size;
 		const c = Math.round(l.code/100);
@@ -54,7 +82,7 @@ aspa.log = (l) => {
 
 const $ip = aspa.ips()[0];
 
-aspa.listen($port, $host, function(err) {  
-	if (err) return ts.error(58,err.stack);
-	ts.println(`Aspa server is running on http://${$ip}:${$port}`,'blue');
+aspa.listen($port, $host, (err) => {  
+	if (err) return ts.error(58,err);
+	ts.println(`Aspa server is running on http://${$ip}:${$port}`,ts.BLUE);
 });
